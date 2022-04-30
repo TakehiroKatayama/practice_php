f34372590ab0e7f5abe0776909413b480ccd90c3
TakehiroKatayama <t.katayama.info@gmail.com>
2022-04-30 15:33:40 +0900

    条件分岐サンプルプログラムを作成

---
+++ conditional.php
@@ -0,0 +1,16 @@
+<?php
+
+//勇者のhp
+$brave_hp = 30;
+//勇者の攻撃力
+$brave_attack = 10;
+//勇者の防御力
+$brave_defense = 5;
+//敵のhp
+$enemy_hp = 30;
+//敵の攻撃力
+$enemy_attack = 5;
+//敵の防御力
+$enemy_defense = 10;
+
+?>
