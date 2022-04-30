05a6e9a7de170c3658f1c24dc6d2dceec17dc42f
TakehiroKatayama <t.katayama.info@gmail.com>
2022-04-30 15:36:59 +0900

    条件分岐を追加

--- conditional.php
+++ conditional.php
@@ -13,4 +13,27 @@ $enemy_attack = 5;
 //敵の防御力
 $enemy_defense = 10;
 
+//敵に与えるダメージの計算
+$enemy_damage = $brave_attack - $enemy_defense;
+
+//敵のHPにダメージを与える
+$enemy_hp = $enemy_hp - $enemy_damage;
+
+//敵に与えるダメージと残りHPの表示
+echo "敵に".$enemy_damage."のダメージを与えた。\n";
+echo "残りHPは".$enemy_hp."だ。";
+
+//残りHPによってリアクションを変える
+if($enemy_hp > 20){
+  echo "敵は元気だ";
+}elseif($enemy_hp > 10){
+  echo "敵はちょっと弱っている";
+}elseif($enemy_hp > 5){
+  echo "敵はかなり弱っている";
+}elseif($enemy_hp > 0){
+  echo "敵は瀕死だ";
+}else{
+  echo "敵はしんだ";
+}
+
 ?>
